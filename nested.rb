028d747151779888e4dc64870dcae0bc9c58a5cd
Tyler Carroll <tylercarroll1611@gmail.com>
2019-09-30 23:15:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -62,6 +62,8 @@ def dennis_ritchies_language
     programmer_hash[:dennis_ritchie][:languages][0]
  end
 
+
+
 def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
@@ -69,10 +71,10 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-programmer_hash[:yukihiro_matsumoto] = {
+programmer_hash[:yukihiro_matsumoto] =
   :known_for => "Ruby", 
-  :languages => ["LISP", "C"], # May need a comma here?
-} 
+  :languages => ["LISP", "C"] # May need a comma here?
+ 
 
 	programmer_hash = 
  		{
